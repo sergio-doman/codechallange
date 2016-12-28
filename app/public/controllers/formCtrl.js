d12b765b5035c85dc413c9b3c4799973acf940e1
Sergiy Domanitskiy <seraphio777+1@gmail.com>
2016-12-28 18:58:01 +0200

    Form fields added

--- app/public/controllers/formCtrl.js
+++ app/public/controllers/formCtrl.js
@@ -9,6 +9,37 @@ angular.module('myApp.form', ['ngRoute'])
   });
 }])
 
-.controller('formCtrl', [function() {
+.controller('formCtrl', ['$scope', function($scope) {
+
+  var model = $scope.model = {
+
+    form: {
+
+      dataDefault: {
+        gender: "",
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: "",
+        age: "",
+        zip: "",
+        termsAccepted: false
+      },
+
+      data: {},
+
+      register: function () {
+        console.log('register !!!');
+      },
+
+      reset: function () {
+        model.form.data = angular.copy(model.form.dataDefault);
+      }
+    }
+
+  };
+
+
+  model.form.reset();
 
 }]);
